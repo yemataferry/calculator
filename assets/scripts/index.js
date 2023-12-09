3c1ec9e43a2d4704a5e0fc85bdf04528bbb1ed00
Yemata Ferry <yemataferry@gmail.com>
2023-12-09 06:10:42 +0100

    html css and javascript set

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,60 +1,62 @@
-let monitor = document.getElementById('screen')
-function clr() {
-   monitor.value = ''
+const monitor = document.getElementById('screen')
+function clr () {
+  monitor.value = ''
 }
+clr()
 
-function del() {
+function del () {
   if (monitor.value.length > 0) {
-  let screenSplit = monitor.value.split('')
-  screenSplit.pop()
-  let screenJoin = screenSplit.value.join('')
-  monitor.value = screenJoin
-}
-}
-
-function plusMinus() {
-const currentNum = parseFloat(monitor.value)
-monitor.value = -currentNum 
-}
-
-function arithmeticOperation(a, b, sign) {
-  if (b == 0 && a / b) {
-    return 'Error: Division by 0 invalid'
-  } else {
-  switch (sign) {
-    case '+':
-      return a + b;
-    case '-':
-      return a - b;
-    case '*':
-      return a * b;
-    case '/':
-      return a / b;
-    case '%':
-      return a % b;
-    case '.':
-      return a . b;
-    case '-/+':
-      return plusMinus();
-    case 'AC':
-      return '';
-    case 'd':
-      return del();
-    default:
-      'No valid operation'
-      break;
+    let screenSplit = monitor.value.split('')
+    screenSplit.pop()
+    const screenJoin = screenSplit.value.join('')
+    monitor.value = screenJoin
   }
 }
-}
-console.log(arithOp(5, 4, '-/+'))
 
-function show(val) {
+function plusMinus () {
+  const currentNum = parseFloat(monitor.value)
+  monitor.value = -currentNum 
+  }
+
+function arithmeticOperation (a, b, sign) {
+  if (b === 0 && a / b) {
+    return 'Error: Division by 0 invalid'
+  } 
+  else {
+    switch (sign) {
+      case '+':
+        return a + b
+      case '-':
+        return a - b
+      case '*':
+        return a * b
+      case '/':
+        return a / b
+      case '%':
+        return a % b
+      case '.':
+        return a . b
+      case '-/+':
+        return plusMinus()
+      case 'AC':
+        return ''
+      case 'd':
+        return del()
+      default:
+        'No valid operation'
+        break
+    }
+  }
+}
+console.log(arithOp(5, 4, '-'))
+
+function show (val) {
   monitor.value += val
 } 
 
 function mathOperation () {
-  let equation = monitor.value
-  let answer = arithmeticOperation(equation)
+  const equation = monitor.value
+  const answer = arithmeticOperation(equation)
   monitor.value = answer
   // let display = document.getElementById('screen').value
 
