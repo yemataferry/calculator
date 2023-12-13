3525864e694aaae1194e2a31c1a3f681842dd9d4
Yemata Ferry <yemataferry@gmail.com>
2023-12-13 09:32:08 +0100

    html css and javascript done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -20,41 +20,42 @@ function plusMinus () {
 }
 
 function arithmeticOperation (a, b, sign) {
-    switch (sign) {
-      case '+':
-        return a + b
-      case '-':
-        return a - b
-      case '*':
-        return a * b
-      case '/':
-        return a / b
-      case '%':
-        return a % b
-      case '.':
-        return a.b
-      case '-/+':
-        return plusMinus()
-      case 'AC':
-        return clr()
-      default:
-        'No valid operation'
-        break
-    }
+  switch (sign) {
+    case '+':
+      return a + b
+    case '-':
+      return a - b
+    case '*':
+      return a * b
+    case '/':
+      return a / b
+    case '%':
+      return a % b
+    case '.':
+      return a.b
+    case '-/+':
+      return plusMinus()
+    case 'AC':
+      return clr()
+    default:
+      monitor.value = 'Error'
+    break
+  }
 }
 // console.log(arithmeticOperation(5, 4, '-'))
 // console.log(arithmeticOperation(1, 0, '/'))
 
-
 function show (val) {
   monitor.value += val
 }
+show('')
 
 function mathOperation () {
   const equation = monitor.value
   const operator = equation.match(/[/*-+%]/)[0]
   const vals = equation.split(operator)
   console.log(vals, operator)
-   let result = arithmeticOperation(+vals[0], +vals[1], operator)
-    monitor.value = result
+  const result = arithmeticOperation(+vals[0], +vals[1], operator)
+  monitor.value = result
 }
+mathOperation()
